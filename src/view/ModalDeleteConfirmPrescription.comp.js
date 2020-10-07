d5d7e9e0b69de9eab792025569652c49521199df
saumya <2saumyaray@gmail.com>
2020-10-07 16:48:56 +0530

    wip: Component Development : Modal Confirmation Component is almost done. Need to add the YES/NO actions

--- src/view/ModalDeleteConfirmPrescription.comp.js
+++ src/view/ModalDeleteConfirmPrescription.comp.js
@@ -12,25 +12,46 @@ const ModalDeleteConfirmPrescription = (props)=>{
         <div className="modal is-active">
             <div className="modal-background"></div>
             <div className="modal-content">
-            <div className="content" style={{background:"#fff"}}>
+                <div className="content" style={{background:"#fff"}}>
 
-                {/* props.data */}
-                
-                <div className="level">
-                    <div className="level-left"> Are you Sure?</div>
-                
-                    <div className="level-right">
-                        <div style={{paddingRight:'0.4em'}}>
-                            <button className="button is-success"> Yes </button> 
+                    {/* props.data */}
+                    
+                    <section className="section">
+                        <div className="container">
+                            <h1 className="title">Are you sure?</h1>
+                            <h2 className="subtitle">You are going to <span className="has-text-danger">Delete</span> it.</h2>
+                            
                         </div>
-                        <div style={{paddingRight:'1em'}}>
-                            <button className="button is-danger"> No </button> 
+                        <div className="level">
+                            <div className="level-left"> </div>
+                            <div className="level-right">
+                                <div style={{paddingRight:'0.4em'}}>
+                                    <button className="button is-success"> Yes </button> 
+                                </div>
+                                <div style={{paddingRight:'1em'}}>
+                                    <button className="button is-danger"> No </button> 
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    
+                    {/*
+                    <div className="level">
+                        <div className="level-left"> Are you Sure?</div>
+                    
+                        <div className="level-right">
+                            <div style={{paddingRight:'0.4em'}}>
+                                <button className="button is-success"> Yes </button> 
+                            </div>
+                            <div style={{paddingRight:'1em'}}>
+                                <button className="button is-danger"> No </button> 
+                            </div>
                         </div>
                     </div>
-                </div>
-                
+                    */}
+                    
 
-            </div>
+                </div>
             </div>
             <button className="modal-close is-large" onClick={onClose}></button>
         </div>
